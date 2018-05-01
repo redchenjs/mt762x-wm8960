15ee0cb480345759d4a0c1c876439ab84962c0d6
redchenjs <redchenjs@live.com>
2018-05-02 00:38:07 +0800

    mt76x8-wm8960: update dapm widget name

--- linux/sound/soc/ralink/mt76x8-wm8960.c
+++ linux/sound/soc/ralink/mt76x8-wm8960.c
@@ -26,8 +26,8 @@
 
 #include "../codecs/wm8960.h"
 
-static const struct snd_soc_dapm_widget mt76x8_wm8960_widgets[] = {
-    SND_SOC_DAPM_HP("Headphone Jack", NULL),
+static const struct snd_soc_dapm_widget mt76x8_wm8960_dapm_widgets[] = {
+    SND_SOC_DAPM_HP("Headphone", NULL),
     SND_SOC_DAPM_SPK("Ext Spk", NULL),
     SND_SOC_DAPM_MIC("Mic", NULL)
 };
@@ -55,60 +55,71 @@ static struct snd_soc_ops mt76x8_wm8960_ops = {
     .hw_params = mt76x8_wm8960_ops_hw_params
 };
 
-static struct snd_soc_dai_link mt76x8_wm8960_dai_link = {
-    .name = "wm8960-codec",
-    .stream_name = "wm8960-hifi",
-    .codec_dai_name = "wm8960-hifi",
-    .dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS,
-    .ops = &mt76x8_wm8960_ops,
-    .dpcm_playback = 1,
-    .dpcm_capture = 1,
+static struct snd_soc_dai_link mt76x8_wm8960_dai_links[] = {
+    {
+        .name = "wm8960-codec",
+        .stream_name = "wm8960-hifi",
+        .cpu_dai_name = "ralink-i2s",
+        .codec_dai_name = "wm8960-hifi",
+        .dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS,
+        .ops = &mt76x8_wm8960_ops,
+        .dpcm_playback = 1,
+        .dpcm_capture = 1
+    }
 };
 
 static struct snd_soc_card mt76x8_wm8960_card = {
     .name = "MT76x8 WM8960 ASoC Card",
     .owner = THIS_MODULE,
-    .dai_link = &mt76x8_wm8960_dai_link,
-    .num_links = 1,
-    .dapm_widgets = mt76x8_wm8960_widgets,
-    .num_dapm_widgets = ARRAY_SIZE(mt76x8_wm8960_widgets),
+    .dai_link = mt76x8_wm8960_dai_links,
+    .num_links = ARRAY_SIZE(mt76x8_wm8960_dai_links),
+    .dapm_widgets = mt76x8_wm8960_dapm_widgets,
+    .num_dapm_widgets = ARRAY_SIZE(mt76x8_wm8960_dapm_widgets)
 };
 
 static int mt76x8_wm8960_machine_probe(struct platform_device *pdev)
 {
     struct snd_soc_card *card = &mt76x8_wm8960_card;
     struct device_node *platform_node, *codec_node;
-    struct platform_device *i2s_pdev;
+    struct platform_device *platform_dev;
     struct i2c_client *codec_dev;
-    int ret;
+    int ret, i;
 
     platform_node = of_parse_phandle(pdev->dev.of_node, "mediatek,platform", 0);
     if (!platform_node) {
         dev_err(&pdev->dev, "property 'platform' missing or invalid\n");
         return -EINVAL;
     }
-    i2s_pdev = of_find_device_by_node(platform_node);
-    if (!i2s_pdev) {
-        dev_err(&pdev->dev, "failed to find SSI platform device\n");
+    platform_dev = of_find_device_by_node(platform_node);
+    if (!platform_dev) {
+        dev_err(&pdev->dev, "failed to find platform device\n");
         return -EINVAL;
     }
-    mt76x8_wm8960_dai_link.platform_of_node = platform_node;
-    mt76x8_wm8960_dai_link.cpu_dai_name = dev_name(&i2s_pdev->dev);
+    for (i = 0; i < card->num_links; i++) {
+        if (mt76x8_wm8960_dai_links[i].platform_name) {
+            continue;
+        }
+        mt76x8_wm8960_dai_links[i].platform_of_node = platform_node;
+    }
 
     card->dev = &pdev->dev;
 
     codec_node = of_parse_phandle(pdev->dev.of_node, "mediatek,audio-codec", 0);
     if (!codec_node) {
-        dev_err(&pdev->dev,
-            "property 'audio-codec' missing or invalid\n");
+        dev_err(&pdev->dev, "property 'audio-codec' missing or invalid\n");
         return -EINVAL;
     }
     codec_dev = of_find_i2c_device_by_node(codec_node);
     if (!codec_dev || !codec_dev->dev.driver) {
-        dev_err(&pdev->dev, "failed to find codec platform device\n");
+        dev_err(&pdev->dev, "failed to find audio codec device\n");
         return -EINVAL;
     }
-    mt76x8_wm8960_dai_link.codec_of_node = codec_node;
+    for (i = 0; i < card->num_links; i++) {
+        if (mt76x8_wm8960_dai_links[i].codec_name) {
+            continue;
+        }
+        mt76x8_wm8960_dai_links[i].codec_of_node = codec_node;
+    }
 
     ret = snd_soc_of_parse_audio_routing(card, "audio-routing");
     if (ret) {
@@ -117,14 +128,15 @@ static int mt76x8_wm8960_machine_probe(struct platform_device *pdev)
     }
 
     ret = devm_snd_soc_register_card(&pdev->dev, card);
-    if (ret)
+    if (ret) {
         dev_err(&pdev->dev, "%s snd_soc_register_card fail %d\n", __func__, ret);
+    }
 
     return ret;
 }
 
 static const struct of_device_id mt76x8_wm8960_machine_dt_match[] = {
-    {.compatible = "mediatek,mt76x8-wm8960-machine",}
+    {.compatible = "mediatek,mt76x8-wm8960-machine"}
 };
 
 static struct platform_driver mt76x8_wm8960_machine = {
